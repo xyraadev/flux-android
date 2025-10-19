e395bb5930d780a4e33a94e5f78a615f4791f828
xyraadev <fluxmessenger@outlook.com>
2025-10-19 21:39:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-from kivy.app import App
-from kivy.uix.label import Label
-
-class TestApp(App):
-    def build(self):
-        return Label(text='FLUX Mobile работает!')
-
-if __name__ == '__main__':
-    TestApp().run()
+print("FLUX Mobile - базовая версия")
